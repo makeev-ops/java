6ee9fe68204b0bf8bb615e521936f76e28df76fe
makeev-ops <Nikita.Makeev@aplana.com>
2020-02-17 11:57:03 +0300

    Задание 4

---
+++ src/com/company/Task4/Main.java
@@ -0,0 +1,11 @@
+package com.company.Task4;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число в бинарном формате: ");
+        String binary = sc.nextLine();
+        int bin = Integer.parseInt(binary, 2);
+        System.out.println(bin);
+    }
+}
