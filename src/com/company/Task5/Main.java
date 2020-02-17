92eeb6d58abb008e935f395e14d3348fe7dd6fef
makeev-ops <Nikita.Makeev@aplana.com>
2020-02-17 12:12:38 +0300

    Задание 4

--- src/com/company/Task5/Main.java
+++ src/com/company/Task5/Main.java
@@ -6,16 +6,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите число : ");
-        String binary = sc.nextLine();
-        int binary2 = Integer.parseInt(binary);
-        System.out.println(binary2 * 2 + " умножение на 2");
-        System.out.println(binary2 * 3 + " умножение на 3");
-        System.out.println(binary2 * 4 + " умножение на 4");
-        System.out.println(binary2 * 5 + " умножение на 5");
-        System.out.println(binary2 * 6 + " умножение на 6");
-        System.out.println(binary2 * 7 + " умножение на 7");
-        System.out.println(binary2 * 8 + " умножение на 8");
-        System.out.println(binary2 * 9 + " умножение на 9");
-        System.out.println(binary2 * 10 + " умножение на 10");
+        String number = sc.nextLine();
+        int num = Integer.parseInt(number);
+        System.out.println(num * 2 + " умножение на 2");
+        System.out.println(num * 3 + " умножение на 3");
+        System.out.println(num * 4 + " умножение на 4");
+        System.out.println(num * 5 + " умножение на 5");
+        System.out.println(num * 6 + " умножение на 6");
+        System.out.println(num * 7 + " умножение на 7");
+        System.out.println(num * 8 + " умножение на 8");
+        System.out.println(num * 9 + " умножение на 9");
+        System.out.println(num * 10 + " умножение на 10");
     }
 }
