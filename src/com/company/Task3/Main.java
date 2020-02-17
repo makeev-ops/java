0277ea6923a1eedaa7e2c0cb845a0a1ee958de41
makeev-ops <Nikita.Makeev@aplana.com>
2020-02-17 12:20:02 +0300

    Задание 3

--- src/com/company/Task3/Main.java
+++ src/com/company/Task3/Main.java
@@ -2,6 +2,6 @@ package com.company.Task3;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
     }
 }
