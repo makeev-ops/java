66d9356467a9099e297e1e70658e2f0df5955e07
makeev-ops <Nikita.Makeev@aplana.com>
2020-02-18 10:53:45 +0300

    Задание 5

--- src/com/company/Task5/Main.java
+++ src/com/company/Task5/Main.java
@@ -6,17 +6,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите число : ");
-        String number = sc.nextLine();
-        int num = Integer.parseInt(number);
-        System.out.println(num + " умножение на 1");
-        System.out.println(num * 2 + " умножение на 2");
-        System.out.println(num * 3 + " умножение на 3");
-        System.out.println(num * 4 + " умножение на 4");
-        System.out.println(num * 5 + " умножение на 5");
-        System.out.println(num * 6 + " умножение на 6");
-        System.out.println(num * 7 + " умножение на 7");
-        System.out.println(num * 8 + " умножение на 8");
-        System.out.println(num * 9 + " умножение на 9");
-        System.out.println(num * 10 + " умножение на 10");
+        String param = sc.nextLine();
+        int number = Integer.parseInt(param);
+        System.out.println(number + " умножениние на 1");
+        System.out.println(number * 2 + " умножение на 2");
+        System.out.println(number * 3 + " умножение на 3");
+        System.out.println(number * 4 + " умножение на 4");
+        System.out.println(number * 5 + " умножение на 5");
+        System.out.println(number * 6 + " умножение на 6");
+        System.out.println(number * 7 + " умножение на 7");
+        System.out.println(number * 8 + " умножение на 8");
+        System.out.println(number * 9 + " умножение на 9");
+        System.out.println(number * 10 + " умножение на 10");
     }
 }
