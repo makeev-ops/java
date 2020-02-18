90e37773d62886370aa5d4a4685ddc6565fbf7ec
makeev-ops <Nikita.Makeev@aplana.com>
2020-02-18 11:03:57 +0300

    Задание 5

--- src/com/company/Task6/Main.java
+++ src/com/company/Task6/Main.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
     public class Main {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            System.out.println("Введите число : ");
+            System.out.println("Введите число x: ");
             double x = sc.nextDouble();
-            System.out.println("Введите число : ");
+            System.out.println("Введите число y: ");
             double y = sc.nextDouble();
-            System.out.println("Введите число : ");
+            System.out.println("Введите число z: ");
             double z = sc.nextDouble();
-            double a = (x+y+z)/3;
-            System.out.println("Среднее арифметическое чисел x,y,z: " + a);
-            int b = (int) (a/2);
-            System.out.println("Деление среднего арифметического на 2 без остатка: " + b);
-            if (b > 3)
+            double average = (x + y + z) / 3;
+            System.out.println("Среднее арифметическое чисел x,y,z: " + average);
+            int division = (int) (average / 2);
+            System.out.println("Деление среднего арифметического на 2 без остатка: " + division);
+            if (division > 3)
             {
                 System.out.println("Программа выполнена корректно");
             }
