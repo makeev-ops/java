8a074de0fd957036093fc410d19848bf455f7f60
makeev-ops <Nikita.Makeev@aplana.com>
2020-02-18 15:54:57 +0300

    Задание 8

--- src/com/company/Task8/Main.java
+++ src/com/company/Task8/Main.java
@@ -1,11 +1,9 @@
 package com.company.Task8;
 public class Main {
     public static void main(String[] args) {
-        int counter = 1;
-        while (counter <= 50)
+        for (int counter = 1; counter <= 50; counter ++)
         {
             System.out.println(counter);
-            counter ++;
         }
     }
 }
