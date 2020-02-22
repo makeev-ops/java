74bd685a8e53c5a021a405edf63551524e80eba7
makeev-ops <Nikita.Makeev@aplana.com>
2020-02-22 12:46:03 +0300

    Задание 9

---
+++ src/com/company/Task9/Main.java
@@ -0,0 +1,10 @@
+package com.company.Task9;
+public class Main {
+    public static void main(String[] args)  {
+        double[] myList = {1.9, 2.9, 3.4, 3.5};
+        for (int i = 0; i < myList.length; i++)
+        {
+            System.out.println("Элемент массива " + i + ", умноженный на 2: " + myList[i] * 2);
+        }
+    }
+}
