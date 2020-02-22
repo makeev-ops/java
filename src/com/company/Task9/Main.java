50bf3b4dc16771e4a8c9f39aea5bac916810a1d3
makeev-ops <Nikita.Makeev@aplana.com>
2020-02-22 17:59:03 +0300

    Задание 9

--- src/com/company/Task9/Main.java
+++ src/com/company/Task9/Main.java
@@ -1,10 +1,27 @@
 package com.company.Task9;
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args)  {
-        double[] myList = {1.9, 2.9, 3.4, 3.5};
-        for (int i = 0; i < myList.length; i++)
+    public static void main(String[] args) {
+        int[] mas = massiv();
+        System.out.print("Вывод элементов массива, умноженного на 2:\n");
+        dopka(mas);
+    }
+    public static int[] massiv() {
+        Scanner ball = new Scanner(System.in);
+        System.out.println("Введите размер массива: ");
+        int foot = ball.nextInt();
+        int[] corner = new int[foot];
+        for (int i = 0; i < foot; i++) {
+            System.out.print("Введите значение" + " " + i + " " + "элемента массива = \n");
+            corner[i] = ball.nextInt();
+        }
+        return corner;
+    }
+    private static void dopka (int[] massiv_1)
+    {
+        for (int i = 0; i < massiv_1.length; i++)
         {
-            System.out.println("Элемент массива " + i + ", умноженный на 2: " + myList[i] * 2);
+            System.out.print("[" + i + "] = " + massiv_1 [i] * 2 + "\n");
         }
     }
 }
