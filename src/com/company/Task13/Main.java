7874680c0277aa49922eafa45bbddef85b3c6635
makeev-ops <Nikita.Makeev@aplana.com>
2020-02-24 18:25:21 +0300

    Задание 13

---
+++ src/com/company/Task13/Main.java
@@ -0,0 +1,27 @@
+package com.company.Task13;
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args)
+    {
+        Scanner stroka = new Scanner(System.in);
+        System.out.println("Введите первую строку: ");
+        String rez = stroka.nextLine();
+        System.out.println("Введите вторую строку: ");
+        String rez_1 = stroka.nextLine();
+        if (rez.length() == rez_1.length())
+        {
+            System.out.println("Введенные строки равны по длине");
+        }
+        else
+        {
+            if (rez.length() > rez_1.length())
+            {
+                System.out.println("Строка с наибольшей длиной: "+ rez);
+            }
+            else
+            {
+                System.out.println("Строка с наибольшей длиной: "+ rez_1);
+            }
+        }
+    }
+}
