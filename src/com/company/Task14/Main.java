489a2947bd07c22d637c30a53a22540a8cc40107
makeev-ops <Nikita.Makeev@aplana.com>
2020-02-26 11:45:56 +0300

    Задание 14

--- src/com/company/Task14/Main.java
+++ src/com/company/Task14/Main.java
@@ -1,47 +1,12 @@
 package com.company.Task14;
 import java.util.Scanner;
 public class Main {
-    public static void main (String[] args)
-    {
-        String element = hello();
-        System.out.println("String"+"   "+"int"+"      "+"double");
-        System.out.println("  "+element+"     "+ chislo(element)+"       "+drob(chislo(element)));
+    public static void main(String[] args) {
+        Scanner sk = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        String S = sk.nextLine();
+        int X = Integer.parseInt(S);
+        double Y = (double)X;
+        System.out.print("string: " + S + "\n" + "int: " + X + "\n" + "double: " + Y + "" + "\n");
     }
-    private static String hello()
-    {
-        Scanner stroka = new Scanner(System.in);
-        String prov = "^-*[0-9]+[.]*[0-9]*$";
-        System.out.println("Введите число с клавиауры: ");
-        String rez = stroka.nextLine();
-        if (rez.matches(prov))
-        {
-            return rez;
-        }
-        else
-        {
-            while (!rez.matches(prov))
-            {
-                System.out.println("Вы ввели не число! Введите число:");
-                rez = stroka.nextLine();
-            }
-            if (rez.contains("."))
-            {
-                return rez.substring(0, rez.indexOf("."));
-            }
-            else
-            {
-                return rez;
-            }
-        }
-    }
-    private static int chislo(String A)
-    {
-        return Integer.parseInt(A);
-    }
-
-    private static double drob(int X)
-    {
-        return X;
-    }
-
 }
