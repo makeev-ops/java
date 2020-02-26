ee1815c3913250b6b2e812afeca0545a7099d5a2
makeev-ops <Nikita.Makeev@aplana.com>
2020-02-26 09:57:04 +0300

    Задание 9

--- src/com/company/Task9/Main.java
+++ src/com/company/Task9/Main.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int[] mas = massiv();
-        System.out.print("Вывод элементов массива, умноженного на 2:\n");
+        System.out.println("Вывод элементов массива, умноженного на 2: ");
         cet(mas);
     }
-    public static int[] massiv() {
+    private static int[] massiv() {
         Scanner ball = new Scanner(System.in);
         System.out.println("Введите размер массива: ");
         int foot = ball.nextInt();
         int[] corner = new int[foot];
         for (int i = 0; i < foot; i++) {
-            System.out.print("Введите значение" + " " + i + " " + "элемента массива = \n");
+            System.out.println("Введите значение" + " " + i + " " + "элемента массива = ");
             corner[i] = ball.nextInt();
         }
         return corner;
@@ -21,7 +21,7 @@ public class Main {
     {
         for (int i = 0; i < massiv_1.length; i++)
         {
-            System.out.print("[" + i + "] = " + massiv_1 [i] * 2 + "\n");
+            System.out.println("[" + i + "] = " + massiv_1 [i] * 2);
         }
     }
 }
