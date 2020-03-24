bca29c706e2441c47651da1f489a9b15afbb9e8f
makeev-ops <Nikita.Makeev@aplana.com>
2020-03-24 15:11:05 +0300

    Задание 16

--- src/com/company/Task16/Main.java
+++ src/com/company/Task16/Main.java
@@ -1,22 +1,20 @@
 package com.company.Task16;
 import java.io.*;
-public class Main
-{
+public class Main {
     public static void main (String agrs[])
     {
         try (FileInputStream text_1 = new FileInputStream("C:\\zadanie.txt"))
         {
             BufferedReader red_for = new BufferedReader(new InputStreamReader(text_1));
-
             String stroka;
             while ((stroka = red_for.readLine()) != null)
             {
                 System.out.println(stroka);
             }
         }
-        catch (IOException e)
+        catch (IOException ext)
         {
-            System.out.println("Ошибка ввода-вывода" + e);
+            System.out.println("Ошибка ввода-вывода" + ext);
         }
     }
 }
