b724b17a5edb5126dfeafbaf389e4a32f30c45c2
makeev-ops <Nikita.Makeev@aplana.com>
2020-03-24 15:11:44 +0300

    Задание 18

--- src/com/company/Task18/Main.java
+++ src/com/company/Task18/Main.java
@@ -1,35 +1,30 @@
 package com.company.Task18;
 import java.io.*;
-public class Main
-{
+public class Main {
     public static void main (String agrs[]) throws IOException
     {
         FileReader map = new FileReader("C:\\zadanie.txt");
         BufferedReader map_1 = new BufferedReader(map);
-
         int i = 0;
         int j = 0;
-
         String stroka;
         while ((stroka = map_1.readLine()) !=null)
         {
             i++;
             System.out.println(stroka);
         }
-        System.out.println("Количество строк в файле для перезаписи = "+i);
+        System.out.println("Количество строк в файле для перезаписи = " + i);
 
         FileWriter rockstar_1 = new FileWriter("C:\\zadanie.txt");
         BufferedWriter stop = new BufferedWriter(rockstar_1);
         BufferedReader crot = new BufferedReader(new InputStreamReader(System.in));
-
         while (j != i)
         {
-            System.out.println("Введите "+(j+1)+" строку в файл");
+            System.out.println("Введите " + (j + 1)+" строку в файл");
             String poker = crot.readLine();
-            stop.write(poker+"\r\n");
+            stop.write(poker + "\r\n");
             j++;
         }
-
         map_1.close();
         crot.close();
         stop.close();
