667a85458bcec4bd18d448ce90a37ef1423e56c6
makeev-ops <Nikita.Makeev@aplana.com>
2020-03-26 18:52:00 +0300

    Задание 17

--- src/com/company/Task17/Main.java
+++ src/com/company/Task17/Main.java
@@ -1,8 +1,10 @@
 package com.company.Task17;
-import java.util.Scanner;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Main {
-    public static void main (String agrs[]) throws IOException
+    public static void main (String[] agrs) throws IOException
     {
         BufferedReader map = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Если хотите закончить ввод данных, то введите слово \"стоп\"");
