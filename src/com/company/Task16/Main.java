78a02fde00f7ccb76cc9a1b4edd50e3e619c783d
makeev-ops <Nikita.Makeev@aplana.com>
2020-03-26 18:49:04 +0300

    Задание 16

--- src/com/company/Task16/Main.java
+++ src/com/company/Task16/Main.java
@@ -1,9 +1,12 @@
 package com.company.Task16;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Main {
-    public static void main (String agrs[])
+    public static void main (String[] agrs)
     {
-        try (FileInputStream text_1 = new FileInputStream("C:\\zadanie.txt"))
+        try (FileInputStream text_1 = new FileInputStream("zadanie.txt"))
         {
             BufferedReader red_for = new BufferedReader(new InputStreamReader(text_1));
             String stroka;
