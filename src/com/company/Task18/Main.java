ea43e99ebc523fdd6ac8562aca1f8039f0f35eaf
makeev-ops <Nikita.Makeev@aplana.com>
2020-03-26 18:50:53 +0300

    Задание 18

--- src/com/company/Task18/Main.java
+++ src/com/company/Task18/Main.java
@@ -1,9 +1,14 @@
 package com.company.Task18;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Main {
-    public static void main (String agrs[]) throws IOException
+    public static void main (String[] agrs) throws IOException
     {
-        FileReader map = new FileReader("C:\\zadanie.txt");
+        FileReader map = new FileReader("zadanie.txt");
         BufferedReader map_1 = new BufferedReader(map);
         int i = 0;
         int j = 0;
@@ -15,7 +20,7 @@ public class Main {
         }
         System.out.println("Количество строк в файле для перезаписи = " + i);
 
-        FileWriter rockstar_1 = new FileWriter("C:\\zadanie.txt");
+        FileWriter rockstar_1 = new FileWriter("zadanie.txt");
         BufferedWriter stop = new BufferedWriter(rockstar_1);
         BufferedReader crot = new BufferedReader(new InputStreamReader(System.in));
         while (j != i)
